d8fa9c65b17d2ca7e567424da3a9285bc118efcd
reavgiel <108542499+reavgiel@users.noreply.github.com>
2023-05-20 13:53:03 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,19 @@ window.onload = function() {
 		$('#social-icons').append(ytLink);
 		$('#social-icons').append(fbLink);
 	  });
+	var swiper = new Swiper('.blog-slider', {
+		spaceBetween: 30,
+		effect: 'fade',
+		loop: true,
+		mousewheel: {
+			invert: false,
+		},
+		// autoHeight: true,
+		pagination: {
+			el: '.blog-slider__pagination',
+			clickable: true,
+		}
+	});
 	let ind = true;
 		window.addEventListener('resize', invision);
 
