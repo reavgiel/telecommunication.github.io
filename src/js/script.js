8bfc8474ff8125d47b0c154dff17ab35e597ba1f
reavgiel <108542499+reavgiel@users.noreply.github.com>
2023-05-20 15:01:26 +0300

    Add files via upload

--- src/js/script.js
+++ src/js/script.js
@@ -26,56 +26,6 @@ window.onload = function() {
 			clickable: true,
 		}
 	});
-	let ind = true;
-		window.addEventListener('resize', invision);
-
-		function invision() {
-			$("a.scroll-button").css("font-size",12+24*((window.innerWidth-360)/(1280-360)));
-			if (window.innerWidth <= 1280 || window.innerHeight/window.innerWidth>0.7) {
-				$('.menu-button').show();
-				if(ind == false){$('.popup-menu').show();}
-				$('#invision').hide();
-				if(ind == false){$('.block').hide();}
-			} else {
-				$('.menu-button').hide();
-				$('.popup-menu').hide();
-				$('#invision').show();
-				if(ind == false){$('.block').show();}
-			}
-			
-		}
-		function createMenuButton() {
-			let menuButton = $('<button class="menu-button"></button>');
-
-			for (let i = 0; i < 3; i++) {
-				let menuButtonLine = $('<div class="menu-button-line"></div>');
-				menuButton.append(menuButtonLine);
-			}
-
-			menuButton.click(function() {
-				ind = !ind;
-				$('.popup-menu').show();
-				$('.block').hide();
-			});
-
-			$('#menu-button-container').append(menuButton);
-			invision();
-		}
-		
-		$('#close-menu-button').click(function() {
-			$('.popup-menu').hide();
-			$('.block').show();
-			ind = !ind;
-		});
-		  // handle click event on scroll buttons
-		  $(".scroll-button").click(function() {
-			  if(ind == false){
-				$('.popup-menu').hide();
-				$('.block').show();
-				ind = !ind;
-			  }
-		});
-		  
 		$(".fa-instagram").mouseenter(function() {$(this).css("text-shadow", "1vh 0.1vh 2vh red");}).mouseleave(function(){$(this).css("text-shadow", "");});
 		createMenuButton();
   };
